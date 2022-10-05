a1d498b7b027eff8c69c0b633699918deebfa9ae
NKspartan <andresnowak52@gmail.com>
2022-10-05 14:39:35 -0500

    Seventh commit

--- etl_csv_v1.py
+++ etl_csv_v1.py
@@ -18,21 +18,22 @@ def feature_selection(df_x: pd.DataFrame, df_y: pd.DataFrame):
 
     pipeline = Pipeline([
         ('scaler', StandardScaler()),
-        ('model', Lasso(tol=1e-2, max_iter=10000))
+        ('clf', Lasso(tol=1e-2, max_iter=5000))
     ])
 
-    feature_selection = GridSearchCV(pipeline,
-                                     {'model__alpha': np.arange(
-                                         0.1, 10, 0.1)},
-                                     cv=5, scoring="neg_mean_squared_error", verbose=0, n_jobs=8)
+    feature_selection = SelectFromModel(GridSearchCV(pipeline,
+                                                     {'clf__alpha': np.arange(
+                                                         0.1, 10, 0.1)},
+                                                     cv=5, scoring="neg_mean_squared_error", verbose=0, n_jobs=6), importance_getter='best_estimator_.named_steps.clf.coef_')
 
     feature_selection.fit(X_train, y_train)
 
-    coefficients = feature_selection.best_estimator_.named_steps['model'].coef_[
-        0]
-    importance = np.abs(coefficients)
+    params = feature_selection.get_support(
+    )
+    print(df_x.loc[:, [not i for i in params]].columns)
 
-    return df_x.loc[:, importance == 0].columns  # columns to remove
+    # columns to remove
+    return df_x.loc[:, [not i for i in params]].columns
 
 
 def feature_combination():
@@ -43,12 +44,19 @@ def remove_atypical_values():
     pass
 
 
+def invalid_correlated_features(df: pd.DataFrame):
+    corr = df.corr()
+
+    return [column for column in corr.columns if pd.isnull(corr[column])[1]]
+
+
 def main(df: pd.DataFrame, options: list):
     data_options = {option: False for option in options}
 
     columns = df.columns
 
     if options["remove_time_features"]:
+        print("Removing time features")
         df = remove_features(df, ['CaptureTime', 'SensorTime'])
 
         data_options["remove_time_features"] = True
@@ -59,6 +67,12 @@ def main(df: pd.DataFrame, options: list):
         df = remove_features(df, columns[last_pos:])
 
         data_options["generic_features"] = True
+    if options["remove_invalid_correlated_features"]:
+        print("Removing invalid correlated features")
+        to_drop = invalid_correlated_features(df)
+        df = remove_features(df, to_drop)
+
+        data_options["remove_invalid_correlated_features"] = True
     if options["feature_combination"]:
         pass
     if options["remove_atypical_values"]:
@@ -78,21 +92,22 @@ def main(df: pd.DataFrame, options: list):
 
     print(data_options)
     df_options = pd.DataFrame(data_options, index=[0])
-    df_options.to_csv("dataset_clean/options_csv_v1_etl.csv")
+    df_options.to_csv("dataset_clean/options_csv_v1_etl.csv", index=False)
 
     return df
 
 
 if __name__ == "__main__":
-    df = pd.read_csv(
+    df_PlatteRiver = pd.read_csv(
         "dataset/2012_2019_PlatteRiverWeir_features_merged_all.csv", index_col=0)
-    df.columns = df.columns.str.replace(' ', '')
+    df_PlatteRiver.columns = df_PlatteRiver.columns.str.replace(' ', '')
 
     # remove the non necessary features
-    df = remove_features(
-        df, ['Filename', 'Agency', 'SiteNumber', 'TimeZone', 'CalcTimestamp', 'width', 'height'])
+    df_PlatteRiver = remove_features(
+        df_PlatteRiver, ['Filename', 'Agency', 'SiteNumber', 'TimeZone', 'CalcTimestamp', 'width', 'height'])
 
-    df = main(df, {'generic_features': True,
-                   'remove_atypical_values': False, 'feature_combination': False, 'remove_feature_selection': True, 'remove_time_features': True})
+    df_PlatteRiver = main(df_PlatteRiver, {'generic_features': False,
+                                           'remove_atypical_values': False, 'feature_combination': False, 'remove_feature_selection': True, 'remove_time_features': True, 'remove_invalid_correlated_features': False})
 
-    df.to_csv("dataset_clean/PlatteRiverWeir_features_v1_clean.csv")
+    df_PlatteRiver.to_csv(
+        "dataset_clean/PlatteRiverWeir_features_v1_clean.csv", index=False)
