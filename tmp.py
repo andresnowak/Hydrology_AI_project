baed88cbf8726b4818fa943388518e5070715446
andres <andresnowak52@gmail.com>
2022-10-12 12:52:13 -0500

    Tenth commit

---
+++ tmp.py
@@ -0,0 +1,13 @@
+import os
+import numpy as np
+import shutil
+
+path = r"dataset/images"
+new_path = r"dataset/images_tmp"
+file_list = os.listdir(path)
+
+files_tmp = np.random.choice(file_list, size=300)
+
+for file in files_tmp:
+    shutil.copy(f"{os.getcwd()}/{path}/{file}",
+                f"{os.getcwd()}/{new_path}/{file}")
