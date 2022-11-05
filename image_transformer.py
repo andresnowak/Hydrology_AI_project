4c5ec5f369454271913028d0f5388e31125d4ee6
andres <andresnowak52@gmail.com>
2022-11-04 21:55:37 -0600

    Thirty eight

--- image_transformer.py
+++ image_transformer.py
@@ -8,6 +8,8 @@ path = r"dataset/images"
 new_path = r"dataset/images_tmp"
 new_path_aug = r"dataset/images_tmp_augmented"
 new_path_draw = r"dataset/images_tmp_draw"
+new_path_draw_sky = r"dataset/images_tmp_draw_sky"
+
 file_list = os.listdir(path)
 
 files_tmp = np.random.choice(file_list, size=20000)
@@ -42,15 +44,28 @@ def image_augmentation(files, seed=None):
     print("finished")
 
 
-def image_painting(files):
+def image_painting_background(files):
     for file in files:
         image = Image.open(f"{os.getcwd()}/{path}/{file}")
         draw = ImageDraw.Draw(image)
 
-        draw.polygon([(0, 0), (0, 200), (512, 150), (512, 0)], fill=(0, 0, 0)) # upper part
-        draw.polygon([(249, 512), (512, 512), (512, 229), (426, 232), (422, 403)], fill=(0, 0, 0)) # bottom part
+        draw.polygon([(0, 0), (0, 200), (512, 150), (512, 0)],
+                     fill=(0, 0, 0))  # upper part
+        draw.polygon([(249, 512), (512, 512), (512, 229),
+                     (426, 232), (422, 403)], fill=(0, 0, 0))  # bottom part
 
         image.save(f"{os.getcwd()}/{new_path_draw}/{file}")
+
+
+def image_painting_sky_and_trees(files):
+    for file in files:
+        image = Image.open(f"{os.getcwd()}/{path}/{file}")
+        draw = ImageDraw.Draw(image)
+
+        draw.polygon([(0, 0), (0, 138), (279, 138), (512, 147),
+                     (512, 0)], fill=(0, 0, 0))  # upper part
+
+        image.save(f"{os.getcwd()}/{new_path_draw_sky}/{file}")
 
 
 def copy(files):
@@ -60,4 +75,4 @@ def copy(files):
 
 
 #image_augmentation(file_list, seed=10)
-image_painting(file_list)
+image_painting_sky_and_trees(file_list)
